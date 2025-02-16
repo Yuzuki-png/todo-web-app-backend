0b86ef7b0beb258c96882bd8a9880628e22ab1cb
YuzukiNishi <youyuexi196@gmail.com>
2025-02-16 22:48:39 +0900

    feat: cors削除

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // ❌ `CORS` の設定を削除
+  // app.enableCors({
+  //   origin: "http://localhost:3000",
+  //   methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  //   allowedHeaders: "Content-Type, Authorization",
+  //   credentials: true,
+  // });
 
-  app.enableCors({
-    origin: "http://localhost:3000",
-    methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
-    credentials: true,
-  });
-
-  await app.listen(3000, "0.0.0.0");
+  await app.listen(3001, "0.0.0.0"); // ✅ NestJS を 3001 で起動
 }
 bootstrap();
